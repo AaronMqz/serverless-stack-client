fe5b0b8e9f88f05cfc5a1b38eeb4f051c79fdc94
Ubuntu <ubuntu@ip-10-0-0-157.ec2.internal>
2019-11-04 22:11:25 +0000

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-s3-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://sgqmxqj6df.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_BoDJFZRQI",
+    APP_CLIENT_ID: "5kogkk5jmbdli6bhqeueuo7nir",
+    IDENTITY_POOL_ID: "us-east-1:c7d5c23f-22db-4fd3-b53f-58b08eaf8c54"
+  }
+};
